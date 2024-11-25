7126df4eb43e0a3532d448ae90a9c5009eaed550
zhixuan <zhixuan@insightfinder.com>
2024-11-25 11:02:28 -0500

    add conditional statements

--- packages/iftracer-sdk/iftracer/sdk/traceutils/set_trace_utils.py
+++ packages/iftracer-sdk/iftracer/sdk/traceutils/set_trace_utils.py
@@ -34,15 +34,42 @@ def _add_model_traces_to_spans(span: Span, res: Dict[str, Any] , *args, **kwargs
     INSIGHTFINDER_ENTITY_EVAL_DURATION + INSIGHTFINDER_ENTITY_LLM_MODEL_NAME
     keys_dict = {key: None for key in keys_list}
     _set_res_dict_values(args, keys_dict, keys_list)
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_PROMPT_TOKEN, _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_PROMPT_TOKEN))
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_EVAL_TOKEN,  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_EVAL_TOKEN))
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_COMPLETION_TOKEN,  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_COMPLETION_TOKEN))
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_TOTAL_TOKEN,  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_TOTAL_TOKEN))
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_ERROR_MESSAGES, _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_ERROR_MESSAGES))
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_TOTAL_DURATION,  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_TOTAL_DURATION))
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_LOAD_DURATION,  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_LOAD_DURATION))
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_EVAL_DURATION,  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_EVAL_DURATION))
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_LLM_MODEL_NAME, _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_LLM_MODEL_NAME))
+    prompt_token = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_PROMPT_TOKEN)
+    if prompt_token is not None:
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_PROMPT_TOKEN, prompt_token)
+
+    eval_token = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_EVAL_TOKEN)
+    if eval_token is not None:
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_EVAL_TOKEN,  eval_token)
+
+    completion_token = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_COMPLETION_TOKEN)
+    if completion_token is not None:
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_COMPLETION_TOKEN,  completion_token)
+
+    total_token = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_TOTAL_TOKEN)
+    if total_token is not None:
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_TOTAL_TOKEN,  total_token)
+
+    error_msg =  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_ERROR_MESSAGES)
+    if error_msg is not None: 
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_ERROR_MESSAGES, error_msg)
+
+    total_duration = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_TOTAL_DURATION)
+    if total_duration is not None: 
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_TOTAL_DURATION, total_duration)
+
+    load_duration = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_LOAD_DURATION)
+    if load_duration is not None:
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_LOAD_DURATION, load_duration)
+
+
+    eval_duration = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_EVAL_DURATION)
+    if eval_duration is not None:
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_EVAL_DURATION,  eval_duration)
+
+    llm_model_name =  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_LLM_MODEL_NAME)
+    if llm_model_name is not None:
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_LLM_MODEL_NAME, llm_model_name)
 
 '''
 Add tags and its value to spans. 
@@ -54,21 +81,37 @@ def _add_result_traces_to_spans(span: Span, res: Dict[str, Any] , *args, **kwarg
     INSIGHTFINDER_ENTITY_RAG_CONFIG
     keys_dict = {key: None for key in keys_list}
     _set_res_dict_values(args, keys_dict, keys_list)
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_ERROR_MESSAGES,  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_ERROR_MESSAGES))
+
+    error_msg = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_ERROR_MESSAGES)
+    if error_msg is not None:
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_ERROR_MESSAGES,  error_msg)
+
     vectorstore =  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_VECTOR_STORE)
-    if vectorstore is not None and vectorstore.embedding_function is not None:
+    if vectorstore is not None and vectorstore.embedding_function is not None and vectorstore.embedding_function.model_name is not None:
         span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_EMBEDDING_MODEL_NAME, vectorstore.embedding_function.model_name )
+
     if vectorstore is not None:    
-        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_VECTOR_STORE_COLLECTION_NAME, vectorstore.collection_name )
-        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_VECTOR_STORE_COLLECTION_METADATA, vectorstore.collection_metadata)
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_DOCS_USED,  _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_DOCS_USED))
-    span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_REDIS_URL, _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_REDIS_URL))
+        if vectorstore.collection_name is not None: 
+            span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_VECTOR_STORE_COLLECTION_NAME, vectorstore.collection_name )
+        if vectorstore.collection_metadata is not None:
+            span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_VECTOR_STORE_COLLECTION_METADATA, vectorstore.collection_metadata)
+
+    docs_used = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_DOCS_USED)
+    if docs_used is not None:
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_DOCS_USED, docs_used)
+        
+    redis_url = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_REDIS_URL)
+    if redis_url is not None:
+        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_REDIS_URL, redis_url)
 
     rag_config = _find_value_from_keys_list(keys_dict, INSIGHTFINDER_ENTITY_RAG_CONFIG)
     if rag_config is not None:
-        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_RAG_CONFIG_COMPANY, rag_config.company )
-        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_RAG_CONFIG_DATASET, rag_config.dataset )
-        span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_RAG_CONFIG_MODEL_FIELDS_SET, rag_config.model_fields_set )
+        if rag_config.company is not None:
+            span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_RAG_CONFIG_COMPANY, rag_config.company )
+        if rag_config.dataset is not None:
+            span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_RAG_CONFIG_DATASET, rag_config.dataset )
+        if rag_config.model_fields_set is not None:
+            span.set_attribute(SpanAttributes.INSIGHTFINDER_ENTITY_RAG_CONFIG_MODEL_FIELDS_SET, rag_config.model_fields_set )
 
 
 
@@ -99,8 +142,6 @@ def _set_res_dict_values_cnt(list, res_dict, keys_list, depth):
                 _set_res_dict_values_cnt(data, res_dict, keys_list, depth+1)
             else:
                 _set_field_value_cnt(data, res_dict, keys_list, 0)
-    # else:
-    #     _set_field_value_cnt(data, res_dict, keys_list, 0)
 
 def _set_field_value_cnt(data, res_dict, keys_list, depth):
     if depth >= MAX_DEPTH or keys_list is None or keys_list == []:
